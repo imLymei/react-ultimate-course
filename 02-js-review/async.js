e7146a0ad8433f9a253fe15509e4208f67ba3ca3
Lymei <lymeicontato@gmail.com>
2023-06-20 17:34:46 -0300

    feat: Asynchronous JavaScript: Async/Await

--- 02-js-review/async.js
+++ 02-js-review/async.js
@@ -1,3 +1,12 @@
-fetch('https://jsonplaceholder.typicode.com/todos').then((res) =>
-	res.json().then((data) => console.log(data))
-);
+// fetch('https://jsonplaceholder.typicode.com/todos').then((res) =>
+// 	res.json().then((data) => console.log(data))
+// );
+
+async function getData() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+	const data = await res.json();
+
+	console.log(data);
+}
+
+getData();
