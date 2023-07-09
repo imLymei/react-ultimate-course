5800dc036ef68830eeac6d251e0c6e89a50a37e6
Lymei <lymeicontato@gmail.com>
2023-07-09 12:35:25 -0300

    feat: Finalized Date Counter

---
+++ challenges/date-counter/src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+
+function App() {
+	const [step, setStep] = useState(1);
+	const [count, setCount] = useState(0);
+	const [date, setDate] = useState(new Date());
+
+	const day = 86400000;
+
+	const isToday = count === 0;
+	const isBeforeToday = count < 0;
+
+	function handleStepChange(isPositive = true) {
+		setStep((step) => (isPositive ? step + 1 : step - 1));
+	}
+
+	function handleCountChange(isPositive = true) {
+		setCount((count) => {
+			const today = date.getTime();
+
+			setDate((date) => new Date(isPositive ? today + day * step : today - day * step));
+			return isPositive ? count + 1 * step : count - 1 * step;
+		});
+	}
+
+	return (
+		<div className='app'>
+			<div className='controls-container'>
+				<div className='buttons-container'>
+					<button onClick={() => handleStepChange(false)}>-</button>
+					<p>step: {step}</p>
+					<button onClick={handleStepChange}>+</button>
+				</div>
+				<div className='buttons-container'>
+					<button onClick={() => handleCountChange(false)}>-</button>
+					<p>count: {count}</p>
+					<button onClick={handleCountChange}>+</button>
+				</div>
+			</div>
+			<p>
+				{isToday
+					? `Today is ${date.toUTCString()}`
+					: isBeforeToday
+					? `${Math.abs(count)} days ago was ${date.toUTCString()}`
+					: `${count} days from today is ${date.toUTCString()}`}
+			</p>
+		</div>
+	);
+}
+
+export default App;
